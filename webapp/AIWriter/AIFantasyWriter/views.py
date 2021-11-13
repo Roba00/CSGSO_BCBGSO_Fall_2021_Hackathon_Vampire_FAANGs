913c631f4ed38ac2add88bdad06639e68084ea78
Parthiv Ganguly <parthiv@iastate.edu>
2021-11-12 18:14:27 -0600

    Added form

--- webapp/AIWriter/AIFantasyWriter/views.py
+++ webapp/AIWriter/AIFantasyWriter/views.py
@@ -6,4 +6,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<h1>Hello, world. You're at the polls index.</h1>")
+    return HttpResponse("<form><input type=\"text\" /> <button type=\"submit\">Submit</button></form>")
