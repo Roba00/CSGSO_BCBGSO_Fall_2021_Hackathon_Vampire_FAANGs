7a3217783847770681a1d0aafa1cadd9f02e9da6
maximpopov11 <icecreamguy0@gmail.com>
2021-11-13 13:56:26 -0600

    App layout

--- androidapp/AI_Writer/app/src/main/java/com/example/ai_writer/MainActivity.java
+++ androidapp/AI_Writer/app/src/main/java/com/example/ai_writer/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.ai_writer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void onClick(View v) {
+        //submit prompt
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
