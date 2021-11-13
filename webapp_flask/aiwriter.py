f39ef05d7baf9384716e358fd849326c1b2679b1
Parthiv Ganguly <parthiv@iastate.edu>
2021-11-13 07:55:02 -0600

    Changed webapp to flask

---
+++ webapp_flask/aiwriter.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+import torch
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/<text>')
+def hello_world(text=None):
+    if (text):
+        tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+        model = GPT2LMHeadModel.from_pretrained('gpt2')
+
+        sequence = text
+        inputs = tokenizer.encode(sequence, return_tensors='pt')
+        outputs = model.generate(inputs, max_length=100, do_sample=True)
+        text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+    return render_template("aiwriter.html", text=text)
